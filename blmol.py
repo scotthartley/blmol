69f3d9eb90b40bcf9a09b7481b3dab3bda4f0316
Scott Hartley <scott.hartley@miamioh.edu>
2019-12-18 17:07:18 -0500

    Fix API changes in Blender 2.8

--- blmol.py
+++ blmol.py
@@ -12,7 +12,7 @@
 
     Some aspects (especially rotating the bond cylinders) based on this
     wonderful blog post from Patrick Fuller:
-    
+
     http://patrick-fuller.com/molecules-from-smiles-molfiles-in-blender/
 
     His project is hosted here:
@@ -20,7 +20,8 @@
     https://github.com/patrickfuller/blender-chemicals
 """
 
-import numpy as np, time
+import numpy as np
+import time
 
 # Python outside of Blender doesn't play all that well with bpy, so need
 # to handle ImportError.
@@ -31,33 +32,33 @@ try:
 except ImportError:
     bpy_avail = False
 
-# Dictionary of color definitions (RGB tuples). Blender RGB colors can
-# be conveniently determined by using a uniform png of the desired color
-# as a background image, then using the eyedropper. Colors with the
-# "_cb" tag are based on colorblind-safe colors as described in this
-# Nature Methods editorial DOI:10.1038/nmeth.1618.
+# Dictionary of color definitions (RGB + alpha tuples). Blender RGB
+# colors can be conveniently determined by using a uniform png of the
+# desired color as a background image, then using the eyedropper. Colors
+# with the "_cb" tag are based on colorblind-safe colors as described in
+# this Nature Methods editorial DOI:10.1038/nmeth.1618.
 COLORS = {
-    'black': (0, 0, 0),
-    'dark_red': (0.55, 0, 0),
-    'gray': (0.2, 0.2, 0.2),
-    'dark_gray': (0.1, 0.1, 0.1),
-    'green': (0.133, 0.545, 0.133),
-    'dark_green': (0.1, 0.5, 0.1),
-    'indigo': (0.294, 0, 0.509),
-    'light_gray': (0.7,0.7,0.7),
-    'orange': (1.0, 0.647, 0),
-    'purple': (0.627, 0.125, 0.941),
-    'red': (0.8, 0, 0),
-    'royal_blue': (0.255, 0.412, 0.882),
-    'white': (1.0, 1.0, 1.0),
-    'yellow': (1.0, 1.0, 0),
-    'blue_cb': (0, 0.168, 0.445),
-    'bluish_green_cb': (0, 0.620, 0.451),
-    'orange_cb': (0.791, 0.347, 0),
-    'reddish_purple_cb': (0.800, 0.475, 0.655),
-    'sky_blue_cb': (0.337, 0.706, 0.914),
-    'vermillion_cb': (0.665, 0.112, 0),
-    'yellow_cb': (0.871, 0.776, 0.054)
+    'black': (0, 0, 0, 1.0),
+    'dark_red': (0.55, 0, 0, 1.0),
+    'gray': (0.2, 0.2, 0.2, 1.0),
+    'dark_gray': (0.1, 0.1, 0.1, 1.0),
+    'green': (0.133, 0.545, 0.133, 1.0),
+    'dark_green': (0.1, 0.5, 0.1, 1.0),
+    'indigo': (0.294, 0, 0.509, 1.0),
+    'light_gray': (0.7, 0.7, 0.7, 1.0),
+    'orange': (1.0, 0.647, 0, 1.0),
+    'purple': (0.627, 0.125, 0.941, 1.0),
+    'red': (0.8, 0, 0, 1.0),
+    'royal_blue': (0.255, 0.412, 0.882, 1.0),
+    'white': (1.0, 1.0, 1.0, 1.0),
+    'yellow': (1.0, 1.0, 0, 1.0),
+    'blue_cb': (0, 0.168, 0.445, 1.0),
+    'bluish_green_cb': (0, 0.620, 0.451, 1.0),
+    'orange_cb': (0.791, 0.347, 0, 1.0),
+    'reddish_purple_cb': (0.800, 0.475, 0.655, 1.0),
+    'sky_blue_cb': (0.337, 0.706, 0.914, 1.0),
+    'vermillion_cb': (0.665, 0.112, 0, 1.0),
+    'yellow_cb': (0.871, 0.776, 0.054, 1.0)
     }
 
 ATOMIC_NUMBERS = {
@@ -78,25 +79,25 @@ ATOMIC_NUMBERS = {
 
 # Dictionary of Van der Waals radii, by atomic number, from Wolfram
 # Alpha.
-RADII = { 
+RADII = {
     1: 1.20,
-    5: 1.92, # From wikipedia
+    5: 1.92,  # From wikipedia
     6: 1.70,
     7: 1.55,
     8: 1.52,
     9: 1.47,
-    12: 1.73, # From wikipedia
+    12: 1.73,  # From wikipedia
     15: 1.80,
     16: 1.80,
     17: 1.75,
-    30: 1.39, # From wikipedia
+    30: 1.39,  # From wikipedia
     35: 1.85,
     53: 1.98
     }
 
 # Dictionaries of colors for drawing elements, by atomic number. Used by
 # several functions when the `color = 'by_element'` option is passed.
-ELEMENT_COLORS = { 
+ELEMENT_COLORS = {
     1: 'white',
     5: 'orange',
     6: 'gray',
@@ -114,34 +115,28 @@ ELEMENT_COLORS = {
 
 # Conversion factors for 1 BU. Default is typically 1 nm. Assumes
 # geometries are input with coords in angstroms.
-UNIT_CONV = { 
+UNIT_CONV = {
     'nm': 0.1,
-    'A': 1.0 
+    'A': 1.0
     }
+
 
 def _create_new_material(name, color):
     """Create a new material.
 
     Args:
         name (str): Name for the new material (e.g., 'red')
-        color (tuple): RGB color for the new material (diffuse_color) 
-            (e.g., (1, 0, 0))
+        color (tuple): RGB color for the new material (diffuse_color)
+            (e.g., (1, 0, 0, 1))
     Returns:
         The new material.
     """
-    
+
     mat = bpy.data.materials.new(name)
     mat.diffuse_color = color
-    mat.diffuse_shader = 'OREN_NAYAR'
-    mat.diffuse_intensity = 0.8
     mat.roughness = 0.5
     mat.specular_color = (1, 1, 1)
-    mat.specular_shader = 'BLINN'
     mat.specular_intensity = 0.2
-    mat.specular_hardness = 25
-    mat.ambient = 1
-    mat.use_transparent_shadows = True
-    # mat.subsurface_scattering.use = True
 
     return mat
 
@@ -151,17 +146,17 @@ class Atom:
 
     Attributes:
         at_num (int): The atomic number of the atom.
-        location (numpy array): The xyz location of the atom, in 
+        location (numpy array): The xyz location of the atom, in
             Angstroms.
         id_num (int): A unique identifier number.
     """
 
     def __init__(self, atomic_number, location, id_num):
         self.at_num = atomic_number
-        self.location = location # np.array
+        self.location = location  # np.array
         self.id_num = id_num
 
-    def draw(self, color='by_element', radius=None, units='nm', 
+    def draw(self, color='by_element', radius=None, units='nm',
              scale=1.0, subsurf_level=2, segments=16):
         """Draw the atom in Blender.
 
@@ -175,7 +170,7 @@ class Atom:
             scale (float, =1.0): Scaling factor for the atom. Useful
                 when generating ball-and-stick models.
             subsurf_level (int, =2): Subsurface subdivisions that will
-            	be applied.
+                be applied.
             segments (int, =16): Number of segments in each UV sphere
                 primitive
 
@@ -194,14 +189,14 @@ class Atom:
 
         # Create sphere as bmesh.
         bm = bmesh.new()
-        bmesh.ops.create_uvsphere(bm, 
-                                  u_segments=segments, 
+        bmesh.ops.create_uvsphere(bm,
+                                  u_segments=segments,
                                   v_segments=segments,
                                   diameter=rad_adj)
-        
+
         for f in bm.faces:
             f.smooth = True
-        
+
         # Convert to mesh.
         me = bpy.data.meshes.new("Mesh")
         bm.to_mesh(me)
@@ -210,8 +205,8 @@ class Atom:
         # Assign mesh to object and place in space.
         atom_sphere = bpy.data.objects.new("atom({})_{}".format(
                             self.at_num, self.id_num), me)
-        bpy.context.scene.objects.link(atom_sphere)
-        
+        bpy.context.collection.objects.link(atom_sphere)
+
         atom_sphere.location = loc_corr
 
         # Assign subsurface modifier, if requested
@@ -225,7 +220,7 @@ class Atom:
         else:
             atom_color = color
 
-        if not atom_color in bpy.data.materials:
+        if atom_color not in bpy.data.materials:
             _create_new_material(atom_color, COLORS[atom_color])
 
         atom_sphere.data.materials.append(bpy.data.materials[atom_color])
@@ -246,7 +241,7 @@ class Bond:
         self.atom2 = atom2
 
     @staticmethod
-    def _draw_half(location, length, rot_angle, rot_axis, element, 
+    def _draw_half(location, length, rot_angle, rot_axis, element,
                    radius=0.2, color='by_element', units='nm',
                    vertices=64, edge_split=False):
         """Draw half of a bond (static method).
@@ -259,7 +254,7 @@ class Bond:
             length (float): The length of the half bond.
             rot_angle (float): Angle by which bond will be rotated.
             rot_axis (np.array): Axis of rotation.
-            element (int): atomic number of element of the bond (for 
+            element (int): atomic number of element of the bond (for
                 coloring).
             radius (float, =0.2): radius of the bond.
             color (string, ='by_element'): color of the bond. If
@@ -280,10 +275,21 @@ class Bond:
         radius_corr = radius * UNIT_CONV[units]
 
         bpy.ops.mesh.primitive_cylinder_add(vertices=vertices,
-                                            radius=radius_corr, 
-                                            depth=len_corr, location=loc_corr, 
+                                            radius=radius_corr,
+                                            depth=len_corr, location=loc_corr,
                                             end_fill_type='NOTHING')
-        bpy.ops.transform.rotate(value=rot_angle, axis=rot_axis)
+
+        # Generate an orientation matrix from rot_axis to handle changes in
+        # Blender's API introduced in v 2.8.
+        rot_matrix_z = rot_axis/np.linalg.norm(rot_axis)
+        rot_matrix_y = np.cross(rot_matrix_z, [0, 0, 1])
+        rot_matrix_x = np.cross(rot_matrix_y, rot_matrix_z)
+        rot_matrix_y = rot_matrix_y/np.linalg.norm(rot_matrix_y)
+        rot_matrix_x = rot_matrix_x/np.linalg.norm(rot_matrix_x)
+        rot_matrix = [rot_matrix_x, rot_matrix_y, rot_matrix_z]
+
+        bpy.ops.transform.rotate(value=rot_angle, orient_axis='Z',
+                                 orient_matrix=rot_matrix)
         bpy.ops.object.shade_smooth()
 
         if edge_split:
@@ -295,14 +301,13 @@ class Bond:
         else:
             bond_color = color
 
-        if not bond_color in bpy.data.materials:
+        if bond_color not in bpy.data.materials:
             _create_new_material(bond_color, COLORS[bond_color])
 
         bpy.context.object.data.materials.append(
             bpy.data.materials[bond_color])
 
         return bpy.context.object
-
 
     def draw(self, radius=0.2, color='by_element', units='nm',
              vertices=64, edge_split=False):
@@ -330,31 +335,32 @@ class Bond:
         length = np.linalg.norm(bond_vector)
 
         bond_axis = bond_vector/length
-        cyl_axis = np.array((0,0,1))
+        cyl_axis = np.array((0, 0, 1))
         rot_axis = np.cross(bond_axis, cyl_axis)
         angle = -np.arccos(np.dot(cyl_axis, bond_axis))
 
         start_center = (self.atom1.location + center_loc)/2
-        created_objects.append(Bond._draw_half(start_center, length/2, angle, 
-                               rot_axis, self.atom1.at_num, radius, color, 
+        created_objects.append(Bond._draw_half(start_center, length/2, angle,
+                               rot_axis, self.atom1.at_num, radius, color,
                                units, vertices, edge_split))
 
         end_center = (self.atom2.location + center_loc)/2
-        created_objects.append(Bond._draw_half(end_center, length/2, angle, 
-                               rot_axis, self.atom2.at_num, radius, color, 
+        created_objects.append(Bond._draw_half(end_center, length/2, angle,
+                               rot_axis, self.atom2.at_num, radius, color,
                                units, vertices, edge_split))
 
+        # Deselect all objects in scene.
         for obj in bpy.context.selected_objects:
-            obj.select = False
-
+            obj.select_set(state=False)
+        # Select all newly created objects.
         for obj in created_objects:
-            obj.select = True
+            obj.select_set(state=True)
 
         bpy.ops.object.join()
         bpy.context.object.name = "bond_{}({})_{}({})".format(
-            self.atom1.id_num, self.atom1.at_num, self.atom2.id_num, 
+            self.atom1.id_num, self.atom1.at_num, self.atom2.id_num,
             self.atom2.at_num)
-        
+
         return bpy.context.object
 
 
@@ -368,15 +374,15 @@ class Molecule:
 
     def __init__(self, name='molecule', atoms=None, bonds=None):
         self.name = name
-        if atoms == None:
+        if atoms is None:
             self.atoms = []
         else:
             self.atoms = atoms
-        if bonds == None:
+        if bonds is None:
             self.bonds = []
         else:
             self.bonds = bonds
-    
+
     def add_atom(self, atom):
         """Adds an atom to the molecule."""
         self.atoms.append(atom)
@@ -384,7 +390,7 @@ class Molecule:
     def add_bond(self, a1id, a2id):
         """Adds a bond to the molecule, using atom ids."""
         if not self.search_bondids(a1id, a2id):
-            self.bonds.append(Bond(self.search_atomid(a1id), 
+            self.bonds.append(Bond(self.search_atomid(a1id),
                                    self.search_atomid(a2id)))
 
     def search_atomid(self, id_to_search):
@@ -404,7 +410,7 @@ class Molecule:
                 return b
         return None
 
-    def draw_bonds(self, caps=True, radius=0.2, color='by_element', 
+    def draw_bonds(self, caps=True, radius=0.2, color='by_element',
                    units='nm', join=True, with_H=True, subsurf_level=1,
                    vertices=64, edge_split=False):
         """Draws the molecule's bonds.
@@ -432,61 +438,66 @@ class Molecule:
             The bonds as a single Blender object, if join=True.
             Otherwise, None.
         """
-        
+
         created_objects = []
 
         for b in self.bonds:
-            if with_H or ( b.atom1.at_num != 1 and b.atom2.at_num != 1 ):
+            if with_H or (b.atom1.at_num != 1 and b.atom2.at_num != 1):
                 created_objects.append(b.draw(radius=radius,
-                                              color=color, 
-                                              units=units, 
+                                              color=color,
+                                              units=units,
                                               vertices=vertices,
                                               edge_split=edge_split))
 
         if caps:
             for a in self.atoms:
                 if with_H or a.at_num != 1:
-                    created_objects.append(a.draw(color=color, 
-                                                  radius=radius, 
+                    created_objects.append(a.draw(color=color,
+                                                  radius=radius,
                                                   units=units,
                                                   subsurf_level=subsurf_level))
-        
-        if join:
-            # Deselect anything currently selected.
-            for obj in bpy.context.selected_objects:
-                obj.select = False
 
+        if join:
+            # # Deselect anything currently selected.
+            # for obj in bpy.context.selected_objects:
+            #     obj.select = False
+
+            # # Select drawn bonds.
+            # for obj in created_objects:
+            #     obj.select = True
+            # Deselect all objects in scene.
+            for obj in bpy.context.selected_objects:
+                obj.select_set(state=False)
             # Select drawn bonds.
             for obj in created_objects:
-                obj.select = True
+                obj.select_set(state=True)
 
             bpy.ops.object.join()
 
             bpy.context.object.name = self.name + '_bonds'
-            
+
             return bpy.context.object
 
         else:
             return None
 
-
-    def draw_atoms(self, color='by_element', radius=None, units='nm', 
+    def draw_atoms(self, color='by_element', radius=None, units='nm',
                    scale=1.0, join=True, with_H=True, subsurf_level=2,
                    segments=16):
         """Draw spheres for all atoms.
 
-        Args: 
+        Args:
             color (str, ='by_element'): If 'by_element', uses colors in
                 ELEMENT_COLORS. Otherwise, can specify color for whole
                 model. Must be defined in COLORS.
-            radius (float, =None): If specified, gives radius of all 
+            radius (float, =None): If specified, gives radius of all
                 atoms.
             units (str, ='nm'): Units for 1 BU. Can also be A.
             join (bool, =True): If true, all atoms are joined together
                 into a single Bl object.
             with_H (bool, =True): Include the hydrogens.
             subsurf_level (int, =2): Subsurface subdivisions that will
-            	be applied to the atoms.
+                be applied to the atoms.
             segments (int, =16): Number of segments in each UV sphere
                 primitive
 
@@ -504,11 +515,11 @@ class Molecule:
 
         # Initiate progress monitor over mouse cursor.
         bpy.context.window_manager.progress_begin(0, len(self.atoms))
-        
+
         n = 0
         for a in self.atoms:
             if with_H or a.at_num != 1:
-                created_objects.append(a.draw(color=color, radius=radius, 
+                created_objects.append(a.draw(color=color, radius=radius,
                                               units=units, scale=scale,
                                               subsurf_level=subsurf_level,
                                               segments=segments))
@@ -521,17 +532,17 @@ class Molecule:
         if join:
             # Deselect all objects in scene.
             for obj in bpy.context.selected_objects:
-                obj.select = False
+                obj.select_set(state=False)
             # Select all newly created objects.
             for obj in created_objects:
-                obj.select = True
-            bpy.context.scene.objects.active = created_objects[0]
+                obj.select_set(state=True)
+
+            bpy.context.view_layer.objects.active = created_objects[0]
             bpy.ops.object.join()
             bpy.context.object.name = self.name + '_atoms'
-            
+
         print("{} seconds".format(time.time()-start_time))
         return
-
 
     def read_pdb(self, filename):
         """Loads a pdb file into a molecule object. Only accepts atoms
@@ -547,14 +558,14 @@ class Molecule:
                 if line[0:4] == "ATOM":
                     idnum = int(line[6:11])
                     atnum = ATOMIC_NUMBERS[line[76:78].strip().upper()]
-                    coords = np.array((float(line[30:38]), float(line[38:46]), 
+                    coords = np.array((float(line[30:38]), float(line[38:46]),
                                        float(line[46:54])))
                     self.add_atom(Atom(atnum, coords, idnum))
 
                 elif line[0:6] == "HETATM":
                     idnum = int(line[6:11])
                     atnum = ATOMIC_NUMBERS[line[76:78].strip().upper()]
-                    coords = np.array((float(line[30:38]), float(line[38:46]), 
+                    coords = np.array((float(line[30:38]), float(line[38:46]),
                                        float(line[46:54])))
                     self.add_atom(Atom(atnum, coords, idnum))
 
